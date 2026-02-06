084e691e3ae7aec2fd726422676188744d16b610
mrhoot101 <moisestomasr51@gmail.com>
2026-02-06 14:31:54 -0500

    Update ChallengeLogic.java

--- ChallengeLogic.java
+++ ChallengeLogic.java
@@ -6,9 +6,15 @@ public class ChallengeLogic {
 
 	public static void main(String[] args) {
 		ArrayList<Payable> paymentQueue = new ArrayList<Payable>();
-		paymentQueue.add(new CreditCard("", 4));
-		paymentQueue.add(new MealPlan ("", 4));
-
+		paymentQueue.add(new CreditCard("David", 100));
+		paymentQueue.add(new MealPlan ("Mary", 500));
+		
+		for (Iterator iterator = paymentQueue.iterator(); iterator.hasNext();) {
+			Payable payable = (Payable) iterator.next();
+			payable.processPayment(50.0);
+		}
+		System.out.println(PaymentMethod.totalTransactions);
 	}
 
 }
+
