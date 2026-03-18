5e81d9d8a724b702d2102d00259f81edafe4c253
mrhoot101 <moisestomasr51@gmail.com>
2026-03-18 14:54:00 -0400

    Add files via upload

---
+++ NullableStudent.kt
@@ -0,0 +1,10 @@
+package Exercise_1
+
+fun main(){
+    val studentName: String
+    var middleName: String? = null?: "No middle"
+
+    studentName = "Moises"
+
+    println("Welcome, $studentName $middleName")
+}
