96d3cbcce97988ec34d47af79a0bfb0f4cf945eb
mrhoot101 <moisestomasr51@gmail.com>
2026-04-23 22:25:40 -0400

    Changed

---
+++ Python/Exercise_1.py
@@ -0,0 +1,36 @@
+# Exercise 1: Basics
+def exercise_1_basics():
+    course = "CMP 269"
+    students = 29
+    print(f"The course {course} has {students} students")
+
+# Exercise 2 : Collecctions
+def exercise_2_collections():
+    color = ["green", "red", "blue", 'yelloe', "brown"]
+    color.append("pink")
+    grade = {
+        "name" : "James",
+        "gpa" : 3.5
+    }
+    print(color)
+    print(grade["name"], grade["gpa"])
+
+# Exercise 3: Logic
+def exercise_3_logic():
+    numbers = [1,2,3,4,5,6,7,8,9,10]
+    events = []
+
+    for num in numbers:
+        if num % 2 == 0:
+            events.append(num)
+    
+    print(events)
+
+name = "  main  "
+if name == "  main  ":
+    print("\n--- Exerice 1 ---")
+    exercise_1_basics()
+    print("\n--- Exerice 2 ---")
+    exercise_2_collections()
+    print("\n--- Exerice 3 ---")
+    exercise_3_logic()
